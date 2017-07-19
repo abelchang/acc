30f99a28d4944ea7e02250f76d5dbf2804813c43
Abel Chang <ccabel42@gmail.com>
2017-07-19 20:26:40 +0800

    add

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -10,3 +10,6 @@
 <script src="{{ asset('bower/jquery/jquery.min.js') }}"></script>
 <script src="{{ asset('bower/bootstrap/dist/js/bootstrap.min.js') }} " integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 <script type="text/javascript" src=" {{ asset('bower/eonasdan-bootstrap-datetimepicker/build/js/bootstrap-datetimepicker.min.js') }} " ></script>
+
+
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
